a8a417e2f2c7caf38d6e8026a04d984567eaca2e
Sinha@532 <lokeshsinha2021@outlook.com>
2024-08-02 00:01:06 +0530

    Update settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn chat_pdf.wsgi:application --log-file -
--- chat_pdf/settings.py
+++ chat_pdf/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,16 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    BASE_DIR / 'chat_pdf/static'
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 #media configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'  # new
@@ -134,4 +136,6 @@ MEDIA_ROOT = BASE_DIR / 'media'  # new
 #authentication URL
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/login/'
+
+django_heroku.settings(locals())
 
